943772366cce963e6a5d2998611e235baefe9e3d
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-06 14:32:02 -0300

    agregar constuctores y metodos getters and setters

--- src/main/java/pronosticos/deportivos/GA_pronosticos_deportivos/entity/Equipo.java
+++ src/main/java/pronosticos/deportivos/GA_pronosticos_deportivos/entity/Equipo.java
@@ -3,4 +3,38 @@ package pronosticos.deportivos.GA_pronosticos_deportivos.entity;
 public class Equipo {
 	private String nombre;
 	private String descripcion;
+
+	/**
+	 * ------- CONSTRUCTORES -------
+	 */
+
+	public Equipo() {
+	}
+
+	public Equipo(String nombre, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	/**
+	 * ------- GETTERS AND SETTERS -------
+	 */
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 }
--- src/main/java/pronosticos/deportivos/GA_pronosticos_deportivos/entity/Ronda.java
+++ src/main/java/pronosticos/deportivos/GA_pronosticos_deportivos/entity/Ronda.java
@@ -5,8 +5,42 @@ import java.util.List;
 public class Ronda {
 	private String nro;
 	private List<Partido> partidos;
-	
+
+	/**
+	 * ------- CONSTRUCTORES -------
+	 */
+
+	public Ronda() {
+	}
+
+	public Ronda(String nro, List<Partido> partidos) {
+		super();
+		this.nro = nro;
+		this.partidos = partidos;
+	}
+
 	public int puntos() {
 		return 0;
 	}
+
+	/**
+	 * ------- GETTERS AND SETTERS -------
+	 */
+
+	public String getNro() {
+		return nro;
+	}
+
+	public void setNro(String nro) {
+		this.nro = nro;
+	}
+
+	public List<Partido> getPartidos() {
+		return partidos;
+	}
+
+	public void setPartidos(List<Partido> partidos) {
+		this.partidos = partidos;
+	}
+
 }
